80837ae1c46463c23e3498369b740dd8924b6c30
Baeonetta <victor.m.kennedy@gmail.com>
2022-03-29 02:35:40 -0500

    made file structure and mocked up HTML and questions

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const inquirer = require('inquirer');
+const Employee = require('./lib/classes')
+const Manager = require('./lib/classes')
+const Engineer = require('./lib/classes')
+const Intern = require('./lib/classes')
 
-// class has to encompass the name, role, and email  
-class Employee{
-    constructor(name, role, email){
-    this.name = name;
-    this.role = role;
-    this.email = email;
-    }
-  getName(){
-      console.log(`${this.name}`)
-  }
-  getRole(){
-      console.log(`${this.role}`)
-  }
-  getEmail(){
-      console.log(`${this.email}`)
-  }
-}
-
- class Manager extends Employee{
-     constructor(phoneNumber){
-     this.phoneNumber = phoneNumber
-     super(name, role, email)
-    }
- }
-
-// Team Builder needs questions to identify role, and roles determine which follow up questions to ask
+// // Team Builder needs questions to identify role, and roles determine which follow up questions to ask
 
 
  inquirer.prompt ([
@@ -36,15 +15,39 @@ class Employee{
         choices: [ 'Manager', 'Engineer', 'Intern' ]
     }
 // if employee is a manager, needs to return the office phone number
-]) .then( response => {
-    if( response.role == 'Manager' ){
-      inquirer.prompt([
-        {
-            name: 'officenumber',
-            type: 'input',
-            message: 'What is your office phone number?'
-        }
-    ])}
+    ]) .then( response => {
+        if( response.role == 'Manager' ){
+        inquirer.prompt([
+             {
+                name: 'officenumber',
+                type: 'number',
+                message: 'What is the office phone number?'
+             }
+             ])}
+            
 // if employee is an engineer, needs to return the github username
-    
-});
+        if( response.role == 'Engineer' ){
+        inquirer.prompt([
+            {
+                name: 'github',
+                type: 'input',
+                message: 'What is your GitHub Username?'
+            }
+         ])}
+        if( response.role == 'Intern' ){
+        inquirer.prompt([
+            {
+                name: 'school',
+                type: 'input',
+                message: 'Where do you attend school if applicable?'
+            }
+         ])}
+
+
+        });
+
+
+        
+// const employee = new Employee('Victor', 'idk', 'Manager', 'email.com')
+
+// employee.getEmail();
