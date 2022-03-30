4d4de3daa01ae98d8bc29b4b14fb7fa335489e4f
Baeonetta <victor.m.kennedy@gmail.com>
2022-03-30 03:39:03 -0500

    made classes into their own files and formatted questions

--- index.js
+++ index.js
@@ -1,53 +1,170 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/classes')
-const Manager = require('./lib/classes')
-const Engineer = require('./lib/classes')
-const Intern = require('./lib/classes')
+const Employee = require('./lib/employee')
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
+const allData = [];
 
 // // Team Builder needs questions to identify role, and roles determine which follow up questions to ask
+function buildTeam(){
 
-
- inquirer.prompt ([
-    {
-        name: 'role',
-        type: 'checkbox',
-        message: 'What type of role is this employee?',
-        choices: [ 'Manager', 'Engineer', 'Intern' ]
-    }
-// if employee is a manager, needs to return the office phone number
-    ]) .then( response => {
-        if( response.role == 'Manager' ){
-        inquirer.prompt([
-             {
-                name: 'officenumber',
-                type: 'number',
-                message: 'What is the office phone number?'
-             }
-             ])}
-            
-// if employee is an engineer, needs to return the github username
-        if( response.role == 'Engineer' ){
-        inquirer.prompt([
+        inquirer.prompt ([
             {
-                name: 'github',
+                name: 'name',
                 type: 'input',
-                message: 'What is your GitHub Username?'
-            }
-         ])}
-        if( response.role == 'Intern' ){
-        inquirer.prompt([
+                message: 'What is the name of this employee?',
+            },
             {
-                name: 'school',
+                name: 'id',
                 type: 'input',
-                message: 'Where do you attend school if applicable?'
+                message: 'What is thier ID',
+            },
+            {
+                name: 'email',
+                type: 'input',
+                message: 'What is their email?',
+            },
+            {
+                name: 'role',
+                type: 'checkbox',
+                message: 'What type of role is this employee?',
+                choices: [ 'Manager', 'Engineer', 'Intern' ]
             }
-         ])}
+        // if employee is a manager, needs to return the office phone number
+            ]) .then( response => {
+                const employee = new Employee(response.name, response.id, response.role, response.email);
+                if( response.role == 'Manager' ){
+                    // console.log(employee)
+                    managerQuestions(employee);
+                }         
+                if( response.role == 'Engineer' ){
+                    engQuestions(employee);
+                }
+                if( response.role == 'Intern' ){
+                    intQuestions(employee);
+                }
 
 
-        });
+                });
 
+}
 
-        
-// const employee = new Employee('Victor', 'idk', 'Manager', 'email.com')
+buildTeam();
 
-// employee.getEmail();
+function managerQuestions(employee){
+    inquirer.prompt([
+        {
+            name: 'officeNumber',
+            type: 'number',
+            message: 'What is the office phone number?'
+        },
+        {
+            name: 'newMember',
+            type: 'confirm',
+            message: 'Would you like to add another team member?'
+        }
+    ])
+        .then( response =>{
+            const manager = new Manager(employee.empName, employee.id, employee.role, employee.email, response.officeNumber)
+            console.log(manager);
+            if (response.newMember == true){
+                allData.push(manager)
+                return buildTeam();
+            } else {
+                allData.push(manager)
+                getHTML(allData)
+            }
+        })
+        }
+function engQuestions(employee){
+    inquirer.prompt([
+        // if employee is an engineer, needs to return the github username
+        {
+            name: 'github',
+            type: 'input',
+            message: 'What is your GitHub Username?'
+        },
+        {
+            name: 'newMember',
+            type: 'confirm',
+            message: 'Would you like to add another team member?'
+        }
+    ])
+        .then( response =>{
+            const engineer = new Engineer(employee.empName, employee.id, employee.role, employee.email, response.github)
+            console.log(engineer);
+            if (response.newMember == true){
+                allData.push(engineer)
+                return buildTeam();
+            } else {
+                allData.push(engineer)
+                getHTML(allData)
+            }
+        })
+        }
+function intQuestions(employee){
+    inquirer.prompt([
+    // intern questions need to ask about the school
+        {
+            name: 'school',
+            type: 'input',
+            message: 'Where do you attend school if applicable?'
+        },
+        {
+            name: 'newMember',
+            type: 'confirm',
+            message: 'Would you like to add another team member?'
+        }
+    ])
+        .then( response =>{
+            const intern = new Intern(employee.empName, employee.id, employee.role, employee.email, response.school)
+            console.log(intern);
+            if (response.newMember == true){
+                allData.push(intern)
+                return buildTeam();
+            } else {
+                allData.push(intern)
+                console.log(allData)
+                buildHTML(allData)
+                
+            }
+        })
+        }
+
+function buildHTML(allData){
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+        <title>Team Builder App</title>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar sticky-top navbar-dark bg-primary">
+                <a class="navbar-brand" href="#!">Team Builder</a>
+              </nav> 
+              <div class="jumbotron">
+                <h1 class="display-3">Let's Get Started</h1>
+                <p class="lead">Your team will output Below</p>
+              </div>     
+        </header>
+        <main>
+            <div class="container">
+            <div class="row">
+            ${allData.forEach(Manager => getHTML(Manager))}
+            ${allData.forEach(Engineer => getHTML(Engineer))}
+            ${allData.forEach(Intern => getHTML(Intern))}
+            </div>         
+    </div>
+    </main>
+
+    
+</body>
+</html>`
+}
+
